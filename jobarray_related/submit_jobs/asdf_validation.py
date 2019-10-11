6251befe3239177cf96c628e23c98f08adb4c095
Ziyi Xi <xiziyi@msu.edu>
2019-10-11 10:39:57 -0400

    add the script to submit validation slurm jobs

---
+++ jobarray_related/submit_jobs/asdf_validation.py
@@ -0,0 +1,89 @@
+import argparse
+import sys
+from glob import glob
+from os.path import join
+
+from slurmpy import Slurm
+
+N_total = 30
+N_each = 3
+N_iter = 10
+nproc = 441
+
+
+def get_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='A python script to submit jobs in one sbatch job',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--base',
+                        help='the directory to place all the specefem directories',
+                        required=True)
+    results = parser.parse_args(args)
+    return results.base
+
+
+def get_dirs(base):
+    return glob(join(base, "*"))
+
+
+def get_scripts(thedirs):
+    result = "date; "
+    result += "module load boost/1.68; "
+    result += "module load phdf5/1.8.16; "
+    # for xmeshfem3D
+    result += f"echo 'start xmeshfem3D'; "
+    for iiter in range(N_iter):
+        result += f"echo 'start iteration {iiter}'; "
+        for ieach in range(N_each):
+            # ievent
+            ievent = iiter*N_each+ieach
+            ievent_dir = thedirs[ievent]
+            # cd
+            result += f"cd {ievent_dir}; "
+            # if N_each-1
+            if(ieach == N_each-1):
+                inc = ieach*nproc
+                result += f"ibrun -n {nproc} -o {inc} ./bin/xmeshfem3D; "
+            else:
+                inc = ieach*nproc
+                result += f"ibrun -n {nproc} -o {inc} ./bin/xmeshfem3D & "
+        result += f"wait; "
+        result += f"echo 'end iteration {iiter}'; "
+        result += f"date; "
+
+    # for xspecfem3D
+    result += f"echo 'start xspecfem3D'; "
+    for iiter in range(N_iter):
+        result += f"echo 'start iteration {iiter}'; "
+        for ieach in range(N_each):
+            # ievent
+            ievent = iiter*N_each+ieach
+            ievent_dir = thedirs[ievent]
+            # cd
+            result += f"cd {ievent_dir}; "
+            # if N_each-1
+            if(ieach == N_each-1):
+                inc = ieach*nproc
+                result += f"ibrun -n {nproc} -o {inc} ./bin/xspecfem3D; "
+            else:
+                inc = ieach*nproc
+                result += f"ibrun -n {nproc} -o {inc} ./bin/xspecfem3D & "
+        result += f"wait; "
+        result += f"echo 'end iteration {iiter}'; "
+        result += f"date; "
+
+    return result
+
+
+def submit_job(thecommand):
+    s = Slurm("sync", {"nodes": 28, "ntasks": 1344,
+                       "partition": 'skx-normal', "time": "06:00:00", "account": "TG-EAR130011"})
+    s.run(thecommand)
+
+
+if __name__ == "__main__":
+    base = get_args(sys.argv[1:])
+    thedirs = get_dirs(base)
+    thecommand = get_scripts(thedirs)
+    submit_job(thecommand)
