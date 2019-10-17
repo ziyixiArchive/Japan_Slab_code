a32fa083dd6a2aeb54bc0a979f29446310b170e1
Ziyi Xi <xiziyi@msu.edu>
2019-10-17 17:55:03 -0400

    debug

--- asdf_related/process/slurm_process_sync_bandpass_icer_test.py
+++ asdf_related/process/slurm_process_sync_bandpass_icer_test.py
@@ -40,7 +40,7 @@ def get_scripts(run_files):
             if(offset >= N_files):
                 continue
             filename = run_files[offset]
-            result += f"srun -N 1 -n {N_cores_each_node} {PY} ../process/process_sync_bandpass.py --min_periods {min_periods} --max_periods {max_periods} --asdf_filename {filename} --waveform_length {waveform_length} --sampling_rate {sampling_rate} --output_directory {PROCESSED_DIR} --logfile {logfile} &"
+            result += f"srun -N 1 -n {N_cores_each_node} --exclusive {PY} ../process/process_sync_bandpass.py --min_periods {min_periods} --max_periods {max_periods} --asdf_filename {filename} --waveform_length {waveform_length} --sampling_rate {sampling_rate} --output_directory {PROCESSED_DIR} --logfile {logfile} &"
         result += f"wait; "
         result += f"echo 'end iteration {iiter}'; "
     return result
