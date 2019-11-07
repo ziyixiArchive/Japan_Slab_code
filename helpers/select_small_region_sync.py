718d1bf17e8584965cb8d32657372dd7d915487e
xiziyi <xiziyi@msu.edu>
2019-11-07 11:58:27 -0500

    fix a typo

--- helpers/select_small_region_sync.py
+++ helpers/select_small_region_sync.py
@@ -9,7 +9,7 @@ output_dir="/scratch/05880/tg851791/sac_files_for_small_region/sync_small"
 all_event_paths=glob(join(sync_dir,"*"))
 all_gcmtids=[basename(item) for item in all_event_paths]
 
-for each_gcmtid in glob(all_gcmtids):
+for each_gcmtid in tqdm(all_gcmtids):
     command=f"mkdir -p {output_dir}/{each_gcmtid}"
     subprocess.call(command,shell=True)
     command=f"mv {sync_dir}/{each_gcmtid}/*sem.sac {output_dir}/{each_gcmtid}"
