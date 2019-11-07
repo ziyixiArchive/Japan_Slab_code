35b015a52dba8fff664025218c893fcfe7562c92
xiziyi <xiziyi@msu.edu>
2019-11-07 13:19:52 -0500

    fix a typo

--- helpers/tar_files_in_dir.py
+++ helpers/tar_files_in_dir.py
@@ -10,7 +10,7 @@ import os
 @click.option('--output_dir', required=True, type=str)
 def main(data_dir,output_dir):
     os.chdir(data_dir)
-    all_data_paths=glob("./*"))
+    all_data_paths=glob(",.*")
     for each_path in tqdm(all_data_paths):
         gcmtid=basename(each_path)
         outpath=join(output_dir,f"{gcmtid}.tar.gz")
